53a4e02e72c9d775a12232d7011fc0aeaa18f7a7
chrisheaththomas@hotmail.com <chrisheaththomas@hotmail.com>
2014-04-07 13:09:29 +0100

    add initial interface and test case

--- README.md
+++ README.md
@@ -1,3 +1,96 @@
 Provides titles for a range of geospatial queries.
 
 /titles?near={"type": "Point","coordinates": [-105.01621,39.57422]}
+
+/titles?contained_by={  "type": "multi_polygon", "coordinates": [[[[30, 20], [45, 40], [10, 40], [30, 20]]], [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]],[[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]]}
+
+Example response body:
+
+{
+
+    "title_number" : "SY938123",
+
+    "address": "123 Fake St",
+
+    "postcode": "ABC12 3CD",
+
+    "registered_owners": [
+
+        {
+
+            "name": "A N Other",
+
+            "address": "123 Fake St"
+
+        },
+
+        {
+
+            "name": "B Other",
+
+            "address": "123 Fake St"
+
+        }
+
+    ],
+
+    "lenders": [
+
+        {
+
+            "name": "Acme Bank PLC"
+
+        }
+
+    ],
+
+    "extent" : 
+
+    {
+
+        "type": "multi_polygon", 
+
+        "coordinates": [
+
+            [
+
+                [[30, 20], [45, 40], [10, 40], [30, 20]]
+
+            ], 
+
+            [
+
+                [[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]
+
+            ]
+
+        ]
+
+    }
+
+}
+
+
+
+POST /titles-revisions
+{
+    "content": {
+        "title_number": "AB1234",
+        "address": "123 Fake St",
+        "extent": {
+            "geometry": {
+                "type": "MultiPolygon",
+                "coordinates": [
+                    [
+                        [
+                            [
+                                14708.755563011973,
+                                6761018.225448865
+                            ]
+                        ]
+                    ]
+                ]
+            }
+        }
+    }
+}
---
+++ app_tests.py
@@ -0,0 +1,93 @@
+import os
+import app
+import unittest
+import tempfile
+
+
+class GeoServiceTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+        app.app.config['TESTING'] = True
+        self.app = app.app.test_client()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(app.app.config['DATABASE'])
+
+    # Polygons URL for test case: https://gist.github.com/anonymous/1abb33df7eae7bdd680b
+    def test_contained_by(self):
+        rv = self.app.post('/titles-revisions', data=json.dumps({
+        "content": {
+            "title_number": "AB1234",
+            "address": "123 Fake St",
+            "extent": {
+                "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [
+                            1.0546875,
+                            16.720385051694
+                        ],
+                        [
+                            3.076171875,
+                            16.214674588248542
+                        ],
+                        [
+                            2.373046875,
+                            15.029685756555674
+                        ],
+                        [
+                            1.0546875,
+                            16.720385051694
+                        ]
+                    ]
+                ]
+            }
+                }
+            }
+        }), content_type='application/json')
+
+        rv = self.app.get('/titles?partially_contained_by={ type": "Polygon", "coordinates": [[[16.5234375, 15.284185114076445], [2.4609375, 13.239945499286312], [-8.4375, 19.973348786110602], [              16.5234375,15.284185114076445]          ]]      }')                         
+
+        self.assertEqual(json.loads(rv.data), {
+                    "content": {                    
+                        "title_number": "AB1234",
+                        "address": "123 Fake St",
+                    "extent": {
+                        "geometry": {
+                        "type": "Polygon",
+                        "coordinates": [
+                            [
+                                [
+                                    1.0546875,
+                                    16.720385051694
+                                ],
+                                [
+                                    3.076171875,
+                                    16.214674588248542
+                                ],
+                                [
+                                    2.373046875,
+                                    15.029685756555674
+                                ],
+                                [
+                                    1.0546875,
+                                    16.720385051694
+                                ]
+                            ]
+                        ]
+                    }
+                        }
+                    }
+                })
+
+
+
+
+
+ 
+
+if __name__ == '__main__':
+    unittest.main()        
