a87c6e1e12de8b25a58c6ebee4623574224cedfa
Ben Firshman <ben@firshman.co.uk>
2014-04-08 15:41:02 +0100

    Add Sentry exception reporting

--- concept_geo_service/settings.py
+++ concept_geo_service/settings.py
@@ -81,3 +81,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+if 'SENTRY_DSN' in os.environ:
+    INSTALLED_APPS = INSTALLED_APPS + (
+        'raven.contrib.django.raven_compat',
+    )
+
+    RAVEN_CONFIG = {
+        'dsn': os.environ['SENTRY_DSN'],
+    }
+
